2e1aa3d4f9acb3e25ef530b3a71dffbeceb7bfd3
RohithRonanki <rohithronanki@gmail.com>
2026-03-12 19:33:19 +0530

    Time: 9 ms (54.96%), Space: 19.5 MB (90.66%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,31 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res=[]
-        def comb(start,currtarget,sub):
-            if currtarget==0:
-                res.append(sub[:])
+        subset=[]
+        def comb(index,currtarget):
+            if currtarget == 0:
+                res.append(subset[:])
                 return
-            for i in range(start,len(candidates)):
-                if candidates[i] <= currtarget:
-                    sub.append(candidates[i])
-                    comb(i,currtarget-candidates[i],sub)
-                    sub.pop()
-        comb(0,target,[])
+            if index == len(candidates) or currtarget<0:
+                return
+            subset.append(candidates[index])
+            comb(index, currtarget-candidates[index])
+            subset.pop()
+            comb(index+1,currtarget)
+        comb(0,target)
         return res
+
+        # res=[]
+        # def comb(start,currtarget,sub):
+        #     if currtarget==0:
+        #         res.append(sub[:])
+        #         return
+        #     for i in range(start,len(candidates)):
+        #         if candidates[i] <= currtarget:
+        #             sub.append(candidates[i])
+        #             comb(i,currtarget-candidates[i],sub)
+        #             sub.pop()
+        # comb(0,target,[])
+        # return res
 
         
